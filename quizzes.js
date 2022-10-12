bc4c8e0b35bd7b3a441c157ae71763fbab883909
juankhusuma <juan.d.khusuma@gmail.com>
2022-10-12 20:56:49 +0700

    session 2

---
+++ quizzes.js
@@ -0,0 +1,106 @@
+const quizzes = [
+  {
+    id: "1",
+    question: "What is the capital of Alberta?",
+    options: [
+      {
+        text: "a",
+        desc: "Edmonton",
+        isAnswer: true,
+      },
+      {
+        text: "b",
+        desc: "Calgary",
+        isAnswer: false,
+      },
+      {
+        text: "c",
+        desc: "Grand Prairie",
+        isAnswer: false,
+      },
+      {
+        text: "d",
+        desc: "Fort McMurray",
+        isAnswer: false,
+      },
+    ],
+  },
+  {
+    id: "2",
+    question: "Where did the name Alberta come from?",
+    options: [
+      {
+        text: "a",
+        desc: "Indigenous peoples of Canada.",
+        isAnswer: false,
+      },
+      {
+        text: "b",
+        desc: "After the queen's daughter.",
+        isAnswer: true,
+      },
+      {
+        text: "c",
+        desc: "This is a description",
+        isAnswer: false,
+      },
+      {
+        text: "d",
+        desc: "None of the above",
+        isAnswer: false,
+      },
+    ],
+  },
+  {
+    id: "3",
+    question: "What is Alberta's provincial flower?",
+    options: [
+      {
+        text: "a",
+        desc: "Wild Rose",
+        isAnswer: true,
+      },
+      {
+        text: "b",
+        desc: "Lady's Slipper",
+        isAnswer: false,
+      },
+      {
+        text: "c",
+        desc: "Fireweed",
+        isAnswer: false,
+      },
+      {
+        text: "d",
+        desc: "Western Red Lily",
+        isAnswer: false,
+      },
+    ],
+  },
+  {
+    id: "4",
+    question: "How many national parks are in Alberta?",
+    options: [
+      {
+        text: "a",
+        desc: "Three",
+        isAnswer: false,
+      },
+      {
+        text: "b",
+        desc: "Two",
+        isAnswer: false,
+      },
+      {
+        text: "c",
+        desc: "Four",
+        isAnswer: false,
+      },
+      {
+        text: "d",
+        desc: "Five",
+        isAnswer: true,
+      },
+    ],
+  },
+];
--- script.js
+++ script.js
@@ -1,93 +1,76 @@
-const quizzes = [
-    {
-        id: "1",
-        question: "lorem ipsum 1",
-        options: [
-            {
-                text: "a",
-                desc: "Sed ut perspiciatis unde omnis",
-                isAnswer: true
-            },
-            {
-                text: "b",
-                desc: "eaque ipsa quae ab illo inventore ve",
-                isAnswer: false
-            },
-            {
-                text: "c",
-                desc: "am aliquam quaerat voluptatem.",
-                isAnswer: false
-            },
-            {
-                text: "d",
-                desc: "Neque porro quisquam est,",
-                isAnswer: false
-            },
-        ]
-    },
-    {
-        id: "2",
-        question: "lorem ipsum 2",
-        options: [
-            {
-                text: "a",
-                desc: "A wonderful serenity has taken possession",
-                isAnswer: false
-            },
-            {
-                text: "b",
-                desc: "like these sweet mornings of spring",
-                isAnswer: true
-            },
-            {
-                text: "c",
-                desc: "whole heart. I am alone, and feel",
-                isAnswer: false
-            },
-            {
-                text: "d",
-                desc: "impenetrable foliage of my trees",
-                isAnswer: false
-            },
-        ]
-    },
-]
+let score = 0;
+let page = 1;
 
-let score = 0
-
-quizzes.forEach(
-    (quiz) => {
-        $("#quizzes").append(`
-            <div class="question" id="q-${quiz.id}">
+quizzes.forEach((quiz) => {
+  $("#quizzes").append(`
+            <div class="question ${quiz.id == page ? "" : "hide"}" id="q-${
+    quiz.id
+  }">
                 <p>${quiz.question}</p>
             </div>
-        `)
+        `);
 
-        quiz.options.forEach(
-            (q) => {
-                $(`#q-${quiz.id}`).append(
-                    `
-                        <div class="show-q-${quiz.id}">
-                            <button class="${ q.isAnswer ? "answer" : "not-answer" } option-${quiz.id}">${q.text}</button>
-                            <span>${q.desc}</span>
-                        </div>
-                    `
-                )
-            }
-        )
+  quiz.options.forEach((q) => {
+    $(`#q-${quiz.id}`).append(
+      `
+        <div class="show-q-${quiz.id}">
+            <button class="${q.isAnswer ? "answer" : "not-answer"} option-${
+        quiz.id
+      }">${q.text}</button>
+            <span>${q.desc}</span>
+        </div>
+    `
+    );
+  });
 
-        $(`.option-${quiz.id}`).on("click", (event) => {
-            $(`.show-q-${quiz.id}`).each(
-                (i, e) => {
-                    $(e).addClass("reveal")
-                }
-            )
+  $(`.option-${quiz.id}`).on("click", (event) => {
+    $(`.show-q-${quiz.id}`).each((i, e) => {
+      $(e).addClass("reveal");
+    });
 
-            if ($(event.target).hasClass("answer")) {
-                score += 1;
-                $("#score").text(score)
-            }
-        })
+    $(".reveal button").attr("disabled", true);
+
+    if ($(event.target).hasClass("answer")) {
+      score += 1;
+      $("#score").text(score);
     }
-)
+  });
+});
 
+$("#next-btn").on("click", (event) => {
+  const questions = $(".question");
+  questions.removeClass("hide");
+  page++;
+  for (let i = 0; i < questions.length; i++) {
+    if (questions[i].id !== `q-${page}`) {
+      $(`#q-${i + 1}`).addClass("hide");
+    }
+  }
+
+  if (page == quizzes.length) {
+    $(event.target).addClass("hide");
+  }
+
+  if (page > 1) {
+    $("#prev-btn").removeClass("hide");
+  }
+});
+
+$("#prev-btn").on("click", (event) => {
+  const questions = $(".question");
+  questions.removeClass("hide");
+  page--;
+  for (let i = 0; i < questions.length; i++) {
+    if (questions[i].id !== `q-${page}`) {
+      $(`#q-${i + 1}`).addClass("hide");
+    }
+  }
+
+  if (page == 1) {
+    $(event.target).addClass("hide");
+  }
+
+  if (page < questions.length) {
+    $("#next-btn").removeClass("hide");
+  }
+});
